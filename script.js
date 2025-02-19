8d05c1035367672629f21d4849f3dce162a9255f
JaredMeeks <jared13meeks56@gmail.com>
2025-02-19 16:20:39 -0500

    added js logic to reset the grid and logic to increase the size of the grid

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let gridSize = 16;
 
 function createGrid(size) {
     container.innerHTML = '';
+    let isDrawing = false;
+
+    document.addEventListener('mousedown', () => isDrawing = true);
+    document.addEventListener('mouseup', () => isDrawing = false);
 
     for (let j = 0; j < size; j++) {
         const row = document.createElement('div');
@@ -13,7 +17,9 @@ function createGrid(size) {
             const div = document.createElement('div');
             div.classList.add('grid-item');
             div.addEventListener('mouseover', () => {
-                div.style.backgroundColor = 'black';
+                if (isDrawing) {
+                    div.style.backgroundColor = 'black';
+                }
             });
             row.appendChild(div);
         }
@@ -22,3 +28,24 @@ function createGrid(size) {
 }
 
 createGrid(gridSize);
+
+const gridSizeBtn = document.querySelector('#size');
+const resetBtn = document.querySelector('#reset');
+
+function gridSizeIncrease(newSize) {
+    gridSize *= 2;
+    createGrid(gridSize);
+
+    if (gridSize === 128) {
+        gridSize = 8;
+    }
+}
+
+function resetGrid() {
+    document.querySelectorAll('.grid-item').forEach(div => {
+        div.style.backgroundColor = 'white';
+    })
+}
+
+gridSizeBtn.addEventListener('click', gridSizeIncrease);
+resetBtn.addEventListener('click', resetGrid);
